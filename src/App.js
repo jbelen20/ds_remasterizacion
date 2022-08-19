cdcb21c41767f3371976a62e0397b83aca787125
conpantuflas <3280kg@gmail.com>
2022-08-19 00:21:09 -0400

    navbar basico estetico listo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dulce sabor</h1>
+      <Navbar />
     </div>
   );
 }
