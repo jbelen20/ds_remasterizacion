791ba4c630f68b23f49f2633b438e9ea3789ef20
jbelen20 <judithcarrenovega@gmail.com>
2024-08-21 14:52:07 -0400

    add styledComponents for footer

---
+++ src/componentsSC/FooterSC.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const ContainFooter = styled.div`
+  background: rgb(152,56,0);
+  background: linear-gradient(0deg, rgba(152,56,0,1) 0%, rgba(215,104,38,1) 20%, rgba(236,136,77,1) 36%, rgba(242,192,163,1) 58%, rgba(251,231,220,1) 71%, rgba(255,255,255,1) 85%);
+  height: 19rem;
+  display: flex;
+  justify-content: space-around;
+`
+
+export const SectiontFooter = styled.div`
+  margin-top: 4rem;
+`
+
+export const Titles = styled.h2`
+  font-family: "Dancing Script", cursive;
+  color:#fff;
+  font-size: 3rem;
+  text-shadow: 1px 1px 1px gray;
+`
+
+export const Descriptions = styled.p`
+  font-family: "Josefin Sans", sans-serif;
+  color:#fff;
+  font-size: 1.3rem;
+  width: 90%;
+`
+export const DescriptionFooterUl = styled.ul`
+  font-family: "Josefin Sans", sans-serif;
+  color:#fff;
+  font-size: 1.3rem;
+`
